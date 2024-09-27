b7eb8319231cb2bd35014f90e144ab667eddedee
Devin Rader <devin@mindwhip.com>
2024-09-27 11:54:49 -0400

    deploy

--- splash-relay/deploy.sh
+++ splash-relay/deploy.sh
@@ -1,12 +1,13 @@
 echo "Checking node.js installation"
-if which node &> /dev/null then
-  echo "node.js installation found"
-else
-  echo "node.js installation not found. Please install node.js."
-  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
-  command -v nvm
-  nvm install node
-fi
+if which node >/dev/null
+  then
+    echo "node.js installation found"
+  else
+    echo "node.js installation not found. Please install node.js."
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+    command -v nvm
+    nvm install node
+  fi
 
 npm install pm2@latest -g
 pm2 update
