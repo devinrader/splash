f860ba2d5d7d7a0c365c8251ea050f4e0e25467b
Devin Rader <devin@mindwhip.com>
2024-09-27 14:06:40 -0400

    deploy

--- splash-relay/deploy.sh
+++ splash-relay/deploy.sh
@@ -1,12 +1,19 @@
 echo "Checking node.js installation"
 which node >/dev/null
+which npm >/dev/null
 
 if which node >/dev/null
   then
     echo "node.js installation found"
   else
-    echo "node.js installation not found. Please install node.js."
-    sudo apt update && sudo apt install -y nodejs npm   
+    echo "node.js installation not found. Install node.js and npm."
+    sudo apt install -y ca-certificates curl gnupg
+    curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /usr/share/keyrings/nodesource.gpg
+    NODE_MAJOR=20
+    echo "deb [signed-by=/usr/share/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+    sudo apt update
+    sudo apt install nodejs
+    node -v
   fi
 
 npm install pm2@latest -g
