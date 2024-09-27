0a2d14f5703facfbaacbcf05ab88c402cd17cbcb
Devin Rader <devin@mindwhip.com>
2024-09-27 11:53:37 -0400

    deploy

--- splash-relay/deploy.sh
+++ splash-relay/deploy.sh
@@ -1,5 +1,5 @@
 echo "Checking node.js installation"
-if command -v node &>/dev/null; then
+if which node &> /dev/null then
   echo "node.js installation found"
 else
   echo "node.js installation not found. Please install node.js."
