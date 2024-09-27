f3ac77a9de9d7198aad609d684dab476103ed642
Devin Rader <devin@mindwhip.com>
2024-09-27 13:10:49 -0400

    deploy

--- splash-relay/deploy.sh
+++ splash-relay/deploy.sh
@@ -2,6 +2,7 @@ echo "Checking node.js installation"
 if which node >/dev/null
   then
     echo "node.js installation found"
+    echo $NVM_DIR
   else
     echo "node.js installation not found. Please install node.js."
     
@@ -10,11 +11,12 @@ if which node >/dev/null
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
     
     export NVM_DIR="$HOME/.nvm"
+    echo $NVM_DIR
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
     [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
     command -v nvm
-    
+
     nvm install node
   fi
 
