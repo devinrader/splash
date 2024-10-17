6c829e9448af3310fe7a19841b5b2a6907748a02
Devin Rader <devin@mindwhip.com>
2024-10-17 14:41:39 -0400

    Simplify build

--- splash-relay/run.sh
+++ splash-relay/run.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 #findusbdev.sh
 
-ls -lha /dev/tty* 
-cat /etc/group
-cat /sys/class/tty/ttyUSB0/dev
-ls -l -R /dev | grep \"188, *0\" 
+#ls -lha /dev/tty* 
+#cat /etc/group
+#cat /sys/class/tty/ttyUSB0/dev
+#ls -l -R /dev | grep \"188, *0\" 
 
-./findusbdev.sh
+#./findusbdev.sh
 
 node dist/main.js --device=/dev/ttyUSB0
 
-npx serialport-terminal --list
+#npx serialport-terminal --list
